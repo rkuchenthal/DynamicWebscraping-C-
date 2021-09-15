9459cc478e2388b6585ab7d2f6327d68d26f5991
rkuchenthal <koochenthal0@gmail.co,=m>
2021-09-15 13:42:16 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.IO;
+using System.Reflection;
+ 
+namespace DynamicWebScrape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape();
+            Console.ReadLine();
+        }
+
+        public static void Scrape()
+        {
+            ChromeOptions options = new ChromeOptions();
+            using (IWebDriver driver = new ChromeDriver(options))
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                driver.Navigate().GoToUrl("https://www.homedepot.com/p/2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776");
+                //driver.FindElement(By.ClassName("price-format__large price-format__main-price"))/*.Click()*/;
+                IWebElement firstResult = wait.Until(ExpectedConditions.ElementExists(By.Id("standard-price")));
+                Console.WriteLine(firstResult.GetAttribute("textContent"));
+            }
+        }
+    }
+}
