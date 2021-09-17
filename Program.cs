81c76f9fc80cdc78421c28f90060c0ea7c09d1a5
rkuchenthal <koochenthal0@gmail.co,=m>
2021-09-17 15:27:35 -0700

    Got it all working. Just need to get values into decimal.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 namespace DynamicWebScrape
 {
 
-    //this class just houses the global variables needed to track the items list and get the new price assigned 
+    //this class just houses the global variables are needed to track the items list and get the new price assigned 
     public class Globals
     {
         public static String newPrice;
@@ -25,23 +25,23 @@ namespace DynamicWebScrape
         static void Main(string[] args)
         {
             
+            //Create the items list for all products
             List<Item> items = new List<Item>();
             items.Add(new Item { Id = 0, ItemPrice = 0, ItemName = "2 in. x 4 in. x 96 in. Prime Whitewood Stud", ItemURL = "2-in-x-4-in-x-96-in-Prime-Whitewood-Stud-058449/312528776", InternetNum = 312528776 });
-            //items.Add(new Item { Id = 1, ItemPrice = 0, ItemName = "2-1/16 in. x 250 ft. Paper Drywall Joint Tape", ItemURL = "USG-Sheetrock-Brand-2-1-16-in-x-250-ft-Paper-Drywall-Joint-Tape-382175/100321613", InternetNum = 100321613 });
-            //items.Add(new Item { Id = 2, ItemPrice = 0, ItemName = "1-1/4 in. x 8 ft. Quicksilver Metal Corner Bead", ItemURL = "ClarkDietrich-1-1-4-in-x-8-ft-Quicksilver-Metal-Corner-Bead-741339/203171469", InternetNum = 203171469 });
+            items.Add(new Item { Id = 1, ItemPrice = 0, ItemName = "2-1/16 in. x 250 ft. Paper Drywall Joint Tape", ItemURL = "USG-Sheetrock-Brand-2-1-16-in-x-250-ft-Paper-Drywall-Joint-Tape-382175/100321613", InternetNum = 100321613 });
+            items.Add(new Item { Id = 2, ItemPrice = 0, ItemName = "1-1/4 in. x 8 ft. Quicksilver Metal Corner Bead", ItemURL = "ClarkDietrich-1-1-4-in-x-8-ft-Quicksilver-Metal-Corner-Bead-741339/203171469", InternetNum = 203171469 });
 
-            //Scrape(items);
             Globals.ItemsCounter = 0;
             foreach (Item item in items)
             {
                 //test, assigned $0 to global so i could test price updates function
-                Globals.newPrice = "$0";
+                //Globals.newPrice = "0";
 
                 //webScraping
                 Scrape(item.ItemURL);
 
                 //test print to check that actual price got assigned to variable
-                Console.WriteLine(Globals.newPrice);
+                //Console.WriteLine(Globals.newPrice);
 
                 //send the info to priceupdate to have the price parsed and added to the list value
                 PriceUpdate(Globals.newPrice, items, Globals.ItemsCounter);
@@ -52,9 +52,11 @@ namespace DynamicWebScrape
             //testing by printing all prices out of items list
             foreach(Item item in items)
             {
+
                 Console.WriteLine(item.ItemPrice);
             }
         }
+
 
         public static void Scrape(String itemURL)
         {
@@ -62,24 +64,31 @@ namespace DynamicWebScrape
 
             using (IWebDriver driver = new ChromeDriver(options))
             {
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
                 
                 driver.Navigate().GoToUrl("https://www.homedepot.com/p/" + itemURL);
                 //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
                 IWebElement firstResult = wait.Until(ExpectedConditions.ElementExists(By.Id("standard-price")));
 
+
                 //test printing/assigning
-                Console.WriteLine(firstResult.GetAttribute("textContent"));
-                String price = firstResult.GetAttribute("textContent");
+                //Console.WriteLine(firstResult.GetAttribute("textContent"));
+                //String price = firstResult.GetAttribute("textContent");
                 //String price1 = firstResult.Text;
 
                 //assigning the price to the global variable
-                Globals.newPrice = firstResult.GetAttribute("textContent");
-                //Globals.newPrice = firstResult.Text;
+                //Globals.newPrice = firstResult.GetAttribute("textContent");
+                Globals.newPrice = firstResult.Text;
+
+
+                while (Globals.newPrice.Equals("") || Globals.newPrice == null)
+                {
+                    Globals.newPrice = firstResult.Text;
+                }
 
                 //test printing
-                Console.WriteLine(Globals.newPrice);
+                //Console.WriteLine(Globals.newPrice);
                 //Console.WriteLine(price);
                  
             }
